2415b131c2ea366ec1f7b8fb35b422d71dfa8726
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-03-08 16:28:53 +0000

    Filter by pipeline status

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -4,8 +4,35 @@ module.exports = function (module) {
     DashboardController.$inject = ['speakerService'];
     function DashboardController(speakerService) {
         var vm = this;
-        speakerService.getSpeakers().then(function (data){
+        speakerService.getSpeakers().then(function (data) {
             vm.speakers = data;
         });
+        vm.excludedStatusesList = new StatusList();
+
+        vm.masterFilterFunction = function(speaker) {
+            return statusFilter(speaker);
+        };
+
+        function statusFilter(speaker) {
+            return !vm.excludedStatusesList.hasStatus(speaker.speakerStatus);
+        }
+
+        function StatusList() {
+            this.statuses = [];
+            this.removeStatus = function(statusToRemove) {
+                this.statuses = this.statuses.filter(function(status) {
+                    return status !== statusToRemove;
+                });
+            };
+            this.addStatus = function(statusToAdd) {
+                this.removeStatus(statusToAdd);
+                this.statuses.push(statusToAdd);
+            };
+            this.hasStatus = function(statusToCheck) {
+                return this.statuses.some(function(status) {
+                    return status === statusToCheck;
+                });
+            };
+        }
     }
 };
--- app/dashboard/sidebar/sidebar.js
+++ app/dashboard/sidebar/sidebar.js
@@ -3,7 +3,23 @@ module.exports = function (module) {
 
     function sidebar() {
         var directive = {
-            templateUrl: "dashboard/sidebar/sidebar.html"
+            scope: {
+                excludedStatusesList: "="
+            },
+            templateUrl: "dashboard/sidebar/sidebar.html",
+            link: function(scope, elem, attrs) {
+                scope.isSelected = function(status) {
+                    return !this.excludedStatusesList.hasStatus(status);
+                };
+                scope.toggleSelected = function(status) {
+                    var wasExcluded = scope.excludedStatusesList.hasStatus(status);
+                    if (wasExcluded) {
+                        scope.excludedStatusesList.removeStatus(status);
+                    } else {
+                        scope.excludedStatusesList.addStatus(status);
+                    }
+                };
+            }
         };
         return directive;
     }
