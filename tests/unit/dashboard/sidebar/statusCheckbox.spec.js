7c0fb022a4fae958223390823440d44d00a225c4
Lauren Clark <lclark@scottlogic.co.uk>
2016-03-09 07:28:47 -0800

    Tests for statusCheckbox

---
+++ tests/unit/dashboard/sidebar/statusCheckbox.spec.js
@@ -0,0 +1,57 @@
+describe('statusCheckbox', function() {
+    beforeEach(angular.mock.module('BristechSRM'));
+    beforeEach(angular.mock.module('template-module'));
+
+    var $compile;
+    var $rootScope;
+
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+    }));
+
+    function compileElement(label, classes, checked, functionName) {
+        var html = "<status-checkbox label=\"" + label
+            + "\" class=\"" + classes
+            + "\" checked=\"" + checked
+            + "\" ng-click=\"" + functionName + "()\"></status-checkbox>";
+        var element = $compile(html)($rootScope);
+        $rootScope.$digest();
+        return element;
+    }
+
+    it("displays label", function() {
+        var element = compileElement("My label", "className", true, "onClick");
+        expect(element.html()).toContain("My label");
+    });
+
+    it("sets rectangle classes", function() {
+        var element = compileElement("My label", "className1 className2", true, "onClick");
+        var rect = getRect(element);
+        var classes = rect.attr("class").split(" ");
+        var containsAll = ["className1", "className2"].every(function(className) {
+            return classes.indexOf(className) !== -1;
+        })
+        expect(containsAll).toBe(true);
+    });
+
+    it("checks checkbox if checked true", function() {
+        var element = compileElement("My label", "className", true, "onClick");
+        var checkbox = getCheckbox(element);
+        expect(checkbox.attr("checked")).toBeTruthy;
+    });
+
+    it("checks checkbox if checked false", function() {
+        var element = compileElement("My label", "className", false, "onClick");
+        var checkbox = getCheckbox(element);
+        expect(checkbox.attr("checked")).toBeFalsy;
+    });
+
+    function getRect(element) {
+        return element.find("rect");
+    }
+
+    function getCheckbox(element) {
+        return element.children().children().find("input");
+    }
+});
