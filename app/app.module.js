ecedc3f9a4934cb36051f9c4713dbf5d743f6c96
Lauren Clark <lclark@scottlogic.co.uk>
2016-03-10 06:52:41 -0800

    Add stars filter test

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,6 @@ var ngRoute = require("angular-route");
 var module = angular.module("BristechSRM",[ngRoute] );
 require("./app.constants.js")(module);
 require("./app.routes.js")(module);
-require('./filters.js')(module);
+require('./filters/stars.filter')(module);
 require('./dashboard')(module);
 require("./services/speaker.service.js")(module);
