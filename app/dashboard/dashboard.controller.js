296fba9ff28e0b15785125e044f760cb45f27c2a
Thomas Hull <thull@scottlogic.co.uk>
2016-03-18 13:48:53 +0000

    Added mapping from speaker to speaker-comms in dashboard

--- app/dashboard/dashboard.controller.js
+++ app/dashboard/dashboard.controller.js
@@ -1,11 +1,22 @@
 module.exports = function (module) {
     module.controller("DashboardController", DashboardController);
 
-    DashboardController.$inject = ['speakerService', 'StatusList', 'SortPreference'];
+    DashboardController.$inject = ['speakerService', 'speakerCommsService', 'StatusList', 'SortPreference'];
     function DashboardController(speakerService, StatusList, SortPreference) {
         var vm = this;
         speakerService.getSpeakers().then(function (data) {
             vm.speakers = data;
+            if(vm.speakerComms !== undefined){
+              mapSpeakerSpeakerComms(data, vm.speakerComms)
+            }
+        });
+
+        speakerCommsService.getSpeakers().then(function (data) {
+            vm.speakerComms = data;
+
+            if(vm.speakers !== undefined) {
+                mapSpeakerSpeakerComms(vm.speakers, data);
+            }
         });
 
         vm.excludedStatusesList = new StatusList();
@@ -17,6 +28,12 @@ module.exports = function (module) {
 
         function statusFilter(speaker) {
             return !vm.excludedStatusesList.hasStatus(speaker.speakerStatus);
+        }
+
+        function mapSpeakerSpeakerComms(speakers, speakerComms) {
+            for(var speaker of speakers) {
+                speaker.lastContacted = speakerComms[speaker.email] !== undefined? speakerComms[speaker.email] : "unkown";
+            }
         }
     }
 };
